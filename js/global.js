f0ac93236886bd76d6ff30021b24dc2478b6135e
carlos henrique da silva melo <carlossilvamelo7@hotmail.com>
2017-05-09 14:12:19 -0300

    refatorações 1

--- js/global.js
+++ js/global.js
@@ -11,29 +11,18 @@ function fecharAbrirChat(item) {
     }
 };
 
+function mudarEstado(item) {
+    var display = document.getElementById(item).style.display;
+    if(display == "none") {
+
+        document.getElementById(item).style.display = 'block';
+    }else {
+
+        document.getElementById(item).style.display = 'none';
+    }
+};
 
 
 
 
-
-
-
-
-
-
-function mudarStyleDisplay(el) {
-    var display = document.getElementById(el).style.display;
-    if(display == "none")
-        document.getElementById(el).style.display = 'block';
-    else
-        document.getElementById(el).style.display = 'none';
-}
-
-function Mudarestado(el) {
-    var display = document.getElementById(el).style.display;
-    if(display == "none")
-        document.getElementById(el).style.display = 'block';
-    else
-        document.getElementById(el).style.display = 'none';
-}
 
